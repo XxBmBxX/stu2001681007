b684a013c768bd510dd729b2551623cdd30124df
XxBmBxX <alexander_1256@abv.bg>
2022-10-25 15:25:59 +0300

    Two tasks done!

---
+++ stu2001681007/DeviceFactory.cs
@@ -0,0 +1,219 @@
+﻿namespace stu2001681007
+{
+    /// <summary>
+    /// Device type enums
+    /// </summary>
+    public enum DeviceType
+    {
+        NONE,
+        LAPTOP,
+        COMPUTER
+    }
+
+    /// <summary>
+    /// Device model enums
+    /// </summary>
+    public enum DeviceModel
+    {
+        NONE,
+        HP,
+        DELL,
+        ASUS,
+    }
+
+    /// <summary>
+    /// Abstract factory class
+    /// </summary>
+    public abstract class DeviceFactory
+    {
+        public abstract Device CreateDevice();
+    }
+
+    /// <summary>
+    /// Concrete factory class for Laptop
+    /// </summary>
+    public class LaptopFactory : DeviceFactory
+    {
+        public override Device CreateDevice()
+        {
+            return new Laptop(DeviceType.LAPTOP, DeviceModel.DELL, 16, false, 3400);
+        }
+    }
+
+
+    /// <summary>
+    /// Concrete factory class for Computer
+    /// </summary>
+    public class ComputerFactory : DeviceFactory
+    {
+        public override Device CreateDevice()
+        {
+            return new Computer(DeviceType.COMPUTER, DeviceModel.ASUS, 8, true, 700);
+        }
+    }
+
+    /// <summary>
+    /// Another level of abstraction to the abstract factory class
+    /// </summary>
+    public class DeviceShop
+    {
+        public static Device OrderDevice(DeviceFactory deviceFactory)
+        {
+            return deviceFactory.CreateDevice();
+        }
+    }
+
+    /// <summary>
+    /// Main abstract object
+    /// </summary>
+    public abstract class Device
+    {
+        #region Private fields
+
+        private readonly DeviceType _deviceType;
+        private readonly DeviceModel _deviceModel;
+        private readonly int _ram;
+        private readonly bool _hasExternalGpu;
+
+        #endregion
+
+        #region Public fields
+
+        public DeviceType DeviceType { get => _deviceType; }
+        public DeviceModel DeviceModel { get => _deviceModel; }
+        public int Ram { get => _ram; }
+        public bool HasExternalGpu { get => _hasExternalGpu; }
+
+        #endregion
+
+        #region Constructors
+
+        public Device()
+        {
+            _deviceType = DeviceType.NONE;
+            _deviceModel = DeviceModel.NONE;
+            _ram = 0;
+            _hasExternalGpu = false;
+        }
+
+        public Device(DeviceType deviceType, DeviceModel deviceModel, int ram, bool hasExternalGpu)
+        {
+            _deviceType = deviceType;
+            _deviceModel = deviceModel;
+            _ram = ram;
+            _hasExternalGpu = hasExternalGpu;
+        }
+
+        #endregion
+
+        public static string BoolToWord(bool boolToConvert)
+        {
+            return boolToConvert ? "Да" : "Не";
+        }
+    }
+
+    /// <summary>
+    /// Laptop class
+    /// </summary>
+    public class Laptop : Device
+    {
+        #region Private fields
+
+        private readonly int _batteryCapacity;
+
+        #endregion
+
+        #region Public fields
+
+        public int BatteryCapacity { get => _batteryCapacity; }
+
+        #endregion
+
+        #region Constructors
+
+        public Laptop() : base()
+        {
+            _batteryCapacity = 0;
+        }
+
+        public Laptop(DeviceType deviceType, DeviceModel deviceModel, int ram, bool hasExternalGpu, int batteryCapacity)
+            : base(deviceType, deviceModel, ram, hasExternalGpu)
+        {
+            _batteryCapacity = batteryCapacity;
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return
+                $"Устройство:\n " +
+                $"Тип: {DeviceType}\n " +
+                $"Модел: {DeviceModel}\n " +
+                $"Рам: {Ram}\n " +
+                $"Външна видео карта: {BoolToWord(HasExternalGpu)}\n " +
+                $"Батерия: {BatteryCapacity}mAh";
+        }
+    }
+
+    /// <summary>
+    /// Computer class
+    /// </summary>
+    public class Computer : Device
+    {
+        #region Private fields
+
+        private readonly int _powerSupplyCapacity;
+
+        #endregion
+
+        #region Public fields
+
+        public int PowerSupplyCapacity { get => _powerSupplyCapacity; }
+
+        #endregion
+
+        #region Constructors
+
+        public Computer() : base()
+        {
+            _powerSupplyCapacity = 0;
+        }
+
+        public Computer(DeviceType deviceType, DeviceModel deviceModel, int ram, bool hasExternalGpu, int powerSupplyCapacity)
+        : base(deviceType, deviceModel, ram, hasExternalGpu)
+        {
+            _powerSupplyCapacity = powerSupplyCapacity;
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return
+                $"Устройство:\n " +
+                $"Тип: {DeviceType}\n " +
+                $"Модел: {DeviceModel}\n " +
+                $"Рам: {Ram}\n " +
+                $"Външна видео карта: {BoolToWord(HasExternalGpu)}\n " +
+                $"Захранване: {PowerSupplyCapacity}W";
+        }
+    }
+
+    /// <summary>
+    /// Main program entry point
+    /// </summary>
+    public class MainDeviceFactoryApp
+    {
+        public static void MainDeviceFactory()
+        {
+            LaptopFactory laptopFactory = new();
+            ComputerFactory computerFactory = new();
+
+            Console.WriteLine(DeviceShop.OrderDevice(laptopFactory));
+            Console.WriteLine(DeviceShop.OrderDevice(computerFactory));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ stu2001681007/FuelPriceObserver.cs
@@ -0,0 +1,119 @@
+﻿namespace stu2001681007
+{
+    public class MainFuelPriceObserverApp
+    {
+        public enum FuelType
+        {
+            Gasoline,
+            Diesel,
+            Ethanol
+        }
+
+        public interface IObserver<T>
+        {
+            void Update(T observedSubject);
+        }
+
+        public class GasStationObserver : IObserver<GasStation>
+        {
+            private string _observerName;
+
+            public string ObserverName { get => _observerName; set => _observerName = value; }
+
+            public GasStationObserver(string observerName)
+            {
+                _observerName = observerName;
+            }   
+
+            public void Update(GasStation observedSubject)
+            {
+                Console.WriteLine($"{ObserverName} беше уведомен, че {observedSubject.StationName} промени цените на горивата си на:\n{observedSubject}");
+            }
+        }
+
+        public abstract class ObservedSubject<T> 
+            where T : ObservedSubject<T>
+        {
+            private readonly List<IObserver<T>> _observers = new();
+
+            public List<IObserver<T>> Observers { get => _observers; }
+
+            public void AttachObserver(IObserver<T> observer)
+            {
+                _observers.Add(observer);
+            }
+
+            public void DetachObserver(IObserver<T> observer)
+            {
+                _observers.Remove(observer);
+            }
+
+            protected void NotifyChange()
+            {
+                _observers.ForEach((observer) => observer.Update((T) this));
+            }
+        }
+
+        public class GasStation : ObservedSubject<GasStation>
+        {
+            private const string Unknown = "unknown";
+            private string _stationName;
+            private (FuelType FuelType, double Price) _gasoline;
+            private (FuelType FuelType, double Price) _diesel;
+            private (FuelType FuelType, double Price) _ethanol;
+           
+            public string StationName { get => _stationName; set => _stationName = value; }
+            public (FuelType FuelType, double Price) Gasoline { get => _gasoline; }
+            public (FuelType FuelType, double Price) Diesel { get => _diesel; }
+            public (FuelType FuelType, double Price) Ethanol { get => _ethanol; }
+
+            public GasStation()
+            {
+                _stationName = Unknown;
+                _gasoline = (FuelType.Gasoline, 0);
+                _diesel = (FuelType.Diesel, 0);
+                _ethanol = (FuelType.Ethanol, 0);
+            }
+
+            public GasStation(string stationName, double gasolinePrice, double dieselPrice, double ethanolPrice)
+            {
+                _stationName = stationName;
+                _gasoline = (FuelType.Gasoline, gasolinePrice);
+                _diesel = (FuelType.Diesel, dieselPrice);
+                _ethanol = (FuelType.Ethanol, ethanolPrice);
+            }
+
+            public void ChangeFuelPrice(FuelType fuelType, int price)
+            {
+                switch (fuelType)
+                {
+                    case FuelType.Gasoline: _gasoline.Price = price; break;
+                    case FuelType.Diesel: _diesel.Price = price; break;
+                    case FuelType.Ethanol: _ethanol.Price = price; break;
+                }
+
+                NotifyChange();
+            }
+
+            public override string ToString()
+            {
+                return $"Цени:\n Бензин: {Gasoline.Price}\n Дизел: {Diesel.Price}\n Етанол: {Ethanol.Price}\n";
+            }
+        }
+
+        public static void Main()
+        {
+            GasStation gasStation = new("Lukoil", 2, 3, 5);
+
+            GasStationObserver gasStationObserver1 = new("Gosho");
+            GasStationObserver gasStationObserver2 = new("Pesho");
+
+            gasStation.AttachObserver(gasStationObserver1);
+            gasStation.AttachObserver(gasStationObserver2);
+
+            gasStation.ChangeFuelPrice(FuelType.Gasoline, 5);
+
+            Console.ReadKey();
+        }
+    }
+}
